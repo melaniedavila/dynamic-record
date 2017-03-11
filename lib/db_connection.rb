5734ba133f81b294f750375ac21244c3719e0290
Melanie Davila <davilam91@gmail.com>
2017-03-11 15:20:03 -0500

    Generalize db_connection to accept different databases

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -1,9 +1,8 @@
 require 'sqlite3'
+require 'yaml'
 
-PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
+PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'false' ? false : true
 ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
-COOKBOOKS_SQL_FILE = File.join(ROOT_FOLDER, 'cookbooks.sql')
-COOKBOOKS_DB_FILE = File.join(ROOT_FOLDER, 'cookbooks.db')
 
 class DBConnection
   def self.open(db_file_name)
@@ -15,13 +14,18 @@ class DBConnection
   end
 
   def self.reset
+    sql_file_absolute_path = File.join(ROOT_FOLDER, sql_file_basename)
+    db_file_absolute_path = sql_file_absolute_path.sub(/sql$/, 'db')
+
+    # We don't need to know if `rm` fails, so we can send any of its
+    # error output to `/dev/null`.
     commands = [
-      "rm '#{COOKBOOKS_DB_FILE}'",
-      "cat '#{COOKBOOKS_SQL_FILE}' | sqlite3 '#{COOKBOOKS_DB_FILE}'"
+      "rm '#{db_file_absolute_path}' 2&>/dev/null",
+      "cat '#{sql_file_absolute_path}' | sqlite3 '#{db_file_absolute_path}'"
     ]
 
     commands.each { |command| `#{command}` }
-    DBConnection.open(COOKBOOKS_DB_FILE)
+    DBConnection.open(db_file_absolute_path)
   end
 
   def self.instance
@@ -42,6 +46,20 @@ class DBConnection
 
   def self.last_insert_row_id
     instance.last_insert_row_id
+  end
+
+  def self.sql_file_basename
+    @sql_file_basename ||= begin
+      database_yml = YAML.load_file('config/database.yml')
+      database_yml['sql_file_basename']
+    end
+  end
+
+  # Below allows the user to set the sql_file_basename without needing to
+  # modify the config file. When getter method is called, @sql_file_basename
+  # will be defined and we will not have to read the config file
+  def self.sql_file_basename=(sql_file_basename)
+    @sql_file_basename = sql_file_basename
   end
 
   private
