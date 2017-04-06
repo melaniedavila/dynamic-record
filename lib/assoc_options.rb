13ec6470da3a7d39829d5c9f8d2211ddf6b48bc6
Melanie Davila <davilam91@gmail.com>
2017-04-06 19:01:57 -0400

    Add comments to AssocOptions class

--- lib/assoc_options.rb
+++ lib/assoc_options.rb
@@ -3,6 +3,7 @@ require 'active_support/inflector'
 class AssocOptions
   attr_accessor :foreign_key, :class_name, :primary_key
 
+  # uses ActiveSupport's inflector library in order to return a class object
   def model_class
     self.class_name.constantize
   end
